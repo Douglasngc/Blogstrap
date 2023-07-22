d1cb4cb93bbe9f2fa73834ea0b2e5ccdfb039a61
Douglasngc <rbdgoncalves19@gmail.com>
2023-07-22 17:18:54 -0300

    Update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,22 @@ class ArticlesController < ApplicationController
     render :new
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+    render :new
+    end
+
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :body)
